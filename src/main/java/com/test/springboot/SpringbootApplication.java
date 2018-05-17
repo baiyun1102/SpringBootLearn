7faee9995b2e151e91022ca725704d82692e6eb3
dong <admin@cxd>
2018-05-17 18:20:22 +0800

    Signed-off-by: dong <admin@cxd>

--- src/main/java/com/test/springboot/SpringbootApplication.java
+++ src/main/java/com/test/springboot/SpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  该注解指定项目为springboot，由此类当作程序入口
  自动装配 web 依赖的环境
- 
+ 注释
 **/
 @SpringBootApplication
 public class SpringbootApplication {
